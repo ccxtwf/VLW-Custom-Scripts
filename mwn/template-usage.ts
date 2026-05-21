87d121d2f2dae6b5d5e93125117efe4d62fdf661
ccxtwf <theweirdfandom@gmail.com>
2026-05-21 15:26:35 +0700

    Add template-sync script for template usage statistics

---
+++ mwn/template-usage.ts
@@ -0,0 +1,92 @@
+import "dotenv/config";
+
+import { Mwn } from "mwn";
+import http from "http";
+import https from "https";
+import axios from "axios";
+import { writeFileSync } from "fs";
+import { integratedLogin } from "./util";
+
+async function initBot() {
+  const bot = new Mwn({
+    apiUrl: process.env.WIKI_API_URL,
+    username: process.env.BOT_USERNAME,
+    password: process.env.BOT_PASSWORD,
+    OAuth2AccessToken: process.env.BOT_OAUTH_ACCESS_TOKEN,
+    userAgent: process.env.BOT_USERAGENT,
+    silent: true,       // suppress messages (except error messages)
+    retryPause: 5000,   // pause for 5000 milliseconds (5 seconds) on maxlag error.
+    maxRetries: 5,      // attempt to retry a failing requests upto 3 times
+    defaultParams: {
+      assert: 'bot',    // assert logged in as bot
+    }
+  });
+
+  if (process.env.ENV_REJECT_UNAUTHORIZED === '0') {
+    console.log("Setting HTTP Request Agent to not reject unauthorized requests. Do not do this on a production environment.");
+    const httpAgent = new http.Agent({ keepAlive: true });
+    const httpsAgent = new https.Agent({ keepAlive: true, rejectUnauthorized: false });
+    axios.defaults.httpAgent = httpAgent;
+    axios.defaults.httpsAgent = httpsAgent;
+    bot.setRequestOptions({ httpAgent, httpsAgent });
+  }
+
+  await integratedLogin(bot);
+  
+  return bot;
+}
+
+interface TemplateUsage { 
+  pageid: number
+  ns: number
+  title: string
+  usage: number
+}
+
+async function main() {
+  const bot = await initBot();
+  const o: Record<number, TemplateUsage> = {};
+  const templateTitles: string[] = (await bot.continuedQuery({
+    action: 'query',
+    format: 'json',
+    list: 'allpages',
+    // apnamespace: '10',
+    apnamespace: '828',
+    aplimit: '500',
+  })).map(({ query: { allpages = [] } = {} }) => allpages.map(({ title }: { title: string }) => title)).flat();
+  // console.log(templateTitles);
+  console.log(`Got ${templateTitles.length} template titles`);
+
+  await bot.batchOperation(
+    templateTitles,
+    async (page) => {
+      const templateUsageGen = bot.continuedQueryGen({
+        action: 'query',
+        format: 'json',
+        prop: 'transcludedin',
+        tiprop: 'pageid', 
+        tishow: '!redirect',
+        tilimit: '500', 
+        tinamespace: '0',
+        titles: page,
+      });
+      for await (const templateUsage of templateUsageGen) {
+        for (const page of templateUsage?.query?.pages || []) {
+          const { pageid, ns, title, transcludedin = [] } = page as { pageid: number, ns: number, title: string, transcludedin?: { pageid: number }[] };
+          if (o[pageid] === undefined) {
+            o[pageid] = { pageid, ns, title, usage: 0 };
+          }
+          o[pageid].usage += transcludedin.length;
+        }
+      }
+    },
+    /* concurrency */ 5,
+    /* retries */ 2
+  );
+
+  const res: TemplateUsage[] = Object.values(o).sort((a, b) => b.usage - a.usage);
+  console.log(`Queried ${res.length} templates`);
+  writeFileSync('./module-usage.log', JSON.stringify(res), { encoding: 'utf-8', flag: 'w' });
+}
+
+main();
